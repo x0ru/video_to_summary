040f091a86591098992d142f01fdcc254f75f806
Bart <bartlomiej.sokol123@gmail.com>
2024-06-18 13:06:09 +0100

    yep

--- download_subtitles.py
+++ download_subtitles.py
@@ -1,25 +1,33 @@
 import yt_dlp
 import srt
 
-url = "https://www.youtube.com/watch?v=w5Wr3j4h_1I"
+def download_sub(url):
+    url = f"{url}"
 
-ydl_opts = {
-  'writeautomaticsub': True,
-  'subtitlesformat': 'srt',
-  'skip_download': True,
-  'outtmpl': 'sub.srt'
-}
+    ydl_opts = {
+      'writeautomaticsub': True,
+      'subtitlesformat': 'srt',
+      'skip_download': True,
+      'outtmpl': 'sub.srt'
+    }
 
-with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-  ydl.download([url])
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        ydl.download([url])
 
 
-with open('sub.srt.en.vtt') as f:
-    for i in range(3):
-        next(f)
-    for line in f:
-        subtitle_generator = srt.parse(f)
-        subtitles = list(subtitle_generator)
-        for sub in subtitles:
-            if "<" not in sub.content:
-                print(sub.content)
+
+def give_me_subs():
+    with open('sub.srt.en.vtt') as f:
+        new_text = ""
+        for i in range(3):
+            next(f)
+        for line in f:
+            subtitle_generator = srt.parse(f)
+            subtitles = list(subtitle_generator)
+            for sub in subtitles:
+                if "<" not in sub.content:
+                    new_text += sub.content
+
+    return new_text
+
+
--- main.py
+++ main.py
@@ -2,12 +2,9 @@ from flask import Flask, render_template, redirect,url_for, session, request
 import secrets
 from wtforms import URLField
 from flask_wtf import FlaskForm, CSRFProtect
-import video
 from flask_executor import Executor
-import time
 import ai_functions
-import os
-import threading
+import download_subtitles
 
 app = Flask(__name__)
 foo = secrets.token_urlsafe(16)
@@ -20,22 +17,14 @@ class PasteVideo(FlaskForm):
     video_link = URLField("Please paste your video link")
 
 
-def check_if_file_downloaded(title):
-    print(title, "calling function")
-    if os.path.isfile(f"{title}.mp4"):
-        transcription, summary = ai_functions.start_ai_transcription(title)
-        return transcription, summary
-    return "Problem with chatgpt", "Problem"
-
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     form = PasteVideo()
     if form.validate_on_submit():
-        title = threading.Thread(target=video.video, args=(form.video_link.data,))
-        time.sleep(20)
-        check_if_file_downloaded(title)
-        return render_template('ulala.html')
+        download_subtitles.download_sub(form.video_link.data)
+        summary = ai_functions.summary()
+        return render_template('ulala.html', summary=summary)
     return render_template('index.html', form=form)
 
 
